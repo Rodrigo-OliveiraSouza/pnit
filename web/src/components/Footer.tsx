78c1ea9eff1b5e56c4735d908628f5cbafbbae57
Rodrigo-OliveiraSouza <rodrigoliveira0001@gmail.com>
2026-01-28 12:20:01 -0300

    Reorder footer logos in single row

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -10,14 +10,19 @@ export default function Footer() {
         <div>
           <div className="footer-logos">
             <img
-              src={`${baseUrl}logos/mir.png`}
-              alt="MIR - Ministério da Igualdade Racial"
-              className="logo logo-mir theme-ignore"
+              src={`${baseUrl}logos/governo-brasil.png`}
+              alt="Governo do Brasil"
+              className="logo logo-governo theme-ignore"
             />
             <img
               src={`${baseUrl}logos/diversifica.png`}
-              alt="Diversifica Inclusão e Diversidade"
+              alt="Diversifica Inclus\u00e3o e Diversidade"
               className="logo logo-diversifica theme-ignore"
+            />
+            <img
+              src={`${baseUrl}logos/mir.png`}
+              alt="MIR - Minist\u00e9rio da Igualdade Racial"
+              className="logo logo-mir theme-ignore"
             />
           </div>
           <p>{footer.description}</p>
@@ -40,13 +45,6 @@ export default function Footer() {
         </div>
       </div>
       <div className="footer-bottom">
-        <div className="footer-logos">
-          <img
-            src={`${baseUrl}logos/governo-brasil.png`}
-            alt="Governo do Brasil"
-            className="logo logo-governo theme-ignore"
-          />
-        </div>
         <span>{footer.version}</span>
       </div>
     </footer>
